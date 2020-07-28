d9ec45e8ff5c1334ce68d984f200e9991cd55a4c
Satish-Masa <kakao2964@gmail.com>
2020-07-28 17:30:12 +0900

    update src/components/BuyItem.vue

---
+++ src/components/BuyItem.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <h4>カートに追加しました。</h4>
+    <div id="btn_home">
+        <b-button size="lg" pill href="/" variant="primary">ホームに戻る</b-button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'BuyItem',
+}
+</script>
+
+<style>
+h4 {
+    text-align: center;
+}
+
+#btn_home {
+    text-align: center;
+    margin-top: 4rem;
+}
+</style>
