88cbe6ba94c6606ebc460c6eaf7534a08fdfca09
Satish-Masa <kakao2964@gmail.com>
2020-08-13 23:07:35 +0900

    update route/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
+import VueSwal from 'vue-swal'
 import HelloWorld from '@/components/HelloWorld'
 import Signin from '@/components/Signin'
 import Signup from '@/components/Signup'
 import Item from '@/components/Item'
 import BuyItem from '@/components/BuyItem'
 import Cart from '@/components/Cart'
+import SuccessMail from '@/components/SuccessMail'
+import FailedMail from '@/components/FailedMail'
 
 Vue.use(Router)
 Vue.use(BootstrapVue)
+Vue.use(VueSwal)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -46,6 +50,16 @@ export default new Router({
       path: '/cart',
       name: 'Cart',
       component: Cart
+    },
+    {
+      path: '/mailcheck',
+      name: "SuccessMail",
+      component: SuccessMail
+    },
+    {
+      path: '/mailcheck/failed',
+      name: "FailedMail",
+      component: FailedMail
     }
   ]
 })
